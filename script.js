06cbdc4eef85041c5cbc6d29aa8882c2c4093367
naomicherno <nonniecheronoh12@gmail.com>
2023-07-07 08:39:26 +0300

    initiate function to fetch json data

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+
+// Fetch data from 'db.json' and process it
+fetch('db.json')
+  .then(response => response.json())
+  .then(data => {
+    displayCards(data.schools, 'boys', 'boys-cards-container');
+    displayCards(data.schools, 'girls', 'girls-cards-container');
+    displayCards(data.schools, 'international', 'international-cards-container');
+  });
