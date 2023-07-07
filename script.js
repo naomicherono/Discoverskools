e51f6e37a7e52203556416d45a215880eebe471a
naomicherno <nonniecheronoh12@gmail.com>
2023-07-07 08:40:47 +0300

    create function to display data in cards

--- script.js
+++ script.js
@@ -22,3 +22,28 @@ fetch('db.json')
     displayCards(data.schools, 'girls', 'girls-cards-container');
     displayCards(data.schools, 'international', 'international-cards-container');
   });
+
+// Display cards based on gender in the specified container
+function displayCards(schools, gender, containerId) {
+  const cardsContainer = document.getElementById(containerId);
+  
+  // Filter schools by gender
+  const filteredSchools = schools.filter(school => school.gender === gender);
+  
+  // Create and append card elements for each filtered school
+  filteredSchools.forEach(school => {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const image = document.createElement('img');
+    image.src = school.image;
+    card.appendChild(image);
+
+    const button = document.createElement('button');
+    button.textContent = 'View More';
+    button.addEventListener('click', () => openModal(school));
+    card.appendChild(button);
+    
+    cardsContainer.appendChild(card);
+  });
+}
